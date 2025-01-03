9c77abeec1e6ee9a585495a20679e72b58c742c6
Sachin Kumar Dagar <sachinkumardagar20001@gmail.com>
2025-01-03 22:20:02 +0530

    Update

--- src/Components/GetApp.tsx
+++ src/Components/GetApp.tsx
@@ -5,10 +5,10 @@ import Button from './Button';
 const GetApp = () => {
   return (
     <section className='flexcenter w-full flex-col pb-[100px]'>
-        <div className='get-app'>
+        <div className="get-app bg-[url('/src/assets/pattern.png')]">
             <div className='z-20 flex w-full flex-1 flex-col items-start justify-center gap-12'>
-                <h1 className="from-neutral-950 lg:bold xl:max-w-[320px]">Get It For Free Now </h1>
-                <p className='regular-16  text-gray-900'>Available on iOS and Android</p>
+                <h1 className="bold-40 lg:bold xl:max-w-[320px]">Get It For Free Now </h1>
+                <p className='regular-16  text-white'>Available on iOS and Android</p>
                 <div className='flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row'>
                 <Button
                 title='App Store'
